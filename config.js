2d4eacf76abdd5ccd2e978ccf15f343895925608
ToxicLeo™ <116261884+Akash-Afx@users.noreply.github.com>
2023-03-10 14:08:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //if the qr have any error, it mean this {whatsapp-bot-pack} package is not up-to-date 
 module.exports = {
-       MONGO_URL : '',//enter your mongodb url to get and store youe session!
-       SESSION_PREFIX : 'inrl~'// defult value of the satrting of your session
+       MONGO_URL : 'mongodb+srv://afx-ToxicLeo:<password>@cluster0.oukc2ds.mongodb.net/?retryWrites=true&w=majority',//enter your mongodb url to get and store youe session!
+       SESSION_PREFIX : 'ToxicLeo~'// defult value of the satrting of your session
        GROUP_URL :"https://chat.whatsapp.com/group?inrl=groupid",//group url for your aupport grp link
        GITHUB_URL : "https://github.com/ekash.efx/inrl/fork"//github id for fork after scaning
 }
